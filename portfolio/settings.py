2dfaac03a1b311ed927d239cb979ab5e75618fb6
Abh-ay <abhayyy1912@gmail.com>
2022-10-03 21:16:53 +0530

    add staticfile storage

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
