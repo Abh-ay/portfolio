a8a17aeb7d8404003ac087faaef1d4704ebba588
Abh-ay <abhayyy1912@gmail.com>
2022-11-04 09:06:15 +0530

    add config for postgresql

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgresql',
-        'USER': config('PG_USER'),
-        'PASSWORD': config('PG_PASSWORD'),
-        'HOST': config('PG_HOST'),
-        'PORT': config('PG_PORT'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #  'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'postgresql',
+    #     'USER': config('PG_USER'),
+    #     'PASSWORD': config('PG_PASSWORD'),
+    #     'HOST': config('PG_HOST'),
+    #     'PORT': config('PG_PORT'),
+    # }
 }
 
 
